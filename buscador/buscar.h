295b9a7e7bde35d1c1637d268634438ee80530ba
alejandro13041 <83832792+alejandro13041@users.noreply.github.com>
2024-04-06 22:19:45 -0500

    Update buscar.h
    add the structures for this code

--- buscador/buscar.h
+++ buscador/buscar.h
@@ -1,19 +1,35 @@
 #ifndef BUSCAR_H
 #define BUSCAR_H
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h> 
-/*
-
-	
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+
+#define size_line 78
+
+struct ip{
+
+    int octet1;
+    int octet2;
+    int octet3;
+    int octet4;
+
+ };
+
+struct line{
+
+    int  lower_rank;
+    int  upper_rank;
+    char country[53];  
+    char code[3];
+    
+ }; 
 
 char RANKS_IP[25] = "IP2LOCATION-LITE-DB1.csv";
 
-int countRows(char*);
-int IPtoString(char*);
-*/
+bool search(int);
+struct ip ip_string(struct ip,char*);
+int database_num(struct ip);
+
 #endif
