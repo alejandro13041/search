d5f5b81390f4bf08c5508f84ff7086b992d97b9c
89p13 <83832792+alejandro13041@users.noreply.github.com>
2026-01-10 16:12:01 -0500

    Update buscar.h
    UPDATE: change the macros to make and update the others options to select others database to search.

--- buscador/buscar.h
+++ buscador/buscar.h
@@ -1,75 +1,89 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * buscar.h - simple IPv4 parsing and database lookup support
- *
- * This header defines the IPv4 structure, range database structure,
- * and helpers used for converting dotted IPv4 strings to numeric form
- * for range-based lookup.
- */
-
 #ifndef _BUSCAR_H
 #define _BUSCAR_H
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+    #include <stdbool.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <stdio.h>
 
-#define SIZE_LINE       78
+    /*
+     * struct ip - IPv4 address split into octets
+     * @octet1: first octet
+     * @octet2: second octet
+     * @octet3: third octet
+     * @octet4: fourth octet
+     */
 
-#define DB1     "IP2LOCATION-LITE-DB1.csv"
-#define DB3     "IP2LOCATION-LITE-DB3.csv"
-#define DB5     "IP2LOCATION-LITE-DB5.csv"
-#define DB9     "IP2LOCATION-LITE-DB9.csv"
-#define DB11    "IP2LOCATION-LITE-DB11.csv"
-
-#define C_DB1   16777216    /* 256^3 */
-#define C_DB2   65536       /* 256^2 */
-#define C_DB3   256         /* 256^1 */
-
-/*
- * struct ip - IPv4 address split into octets
- * @octet1: first octet
- * @octet2: second octet
- * @octet3: third octet
- * @octet4: fourth octet
- */
-struct ip {
+    typedef struct ip{
         int octet1;
         int octet2;
         int octet3;
         int octet4;
-};
+     };
 
-/*
- * struct line - single range entry for database lookup
- * @lower_rank: start of IP range (numeric)
- * @upper_rank: end of IP range (numeric)
- * @country:    country name
- * @code:       2-letter country code
- * @latitude:   latitude (for DB5/DB9/DB11)
- * @longitude:  longitude (for DB5/DB9/DB11)
- */
+     /*
+      * constants to search on database, this 
+      * is important because is the form that 
+      * search in database.
+      */ 
 
-struct line {
-        int   lower_rank;
-        int   upper_rank;
-        char  country[53];
-        char  code[3];
-        float latitude;
-        float longitude;
-};
+    #define C_DB1   16777216    
+    #define C_DB2   65536       
+    #define C_DB3   256         
 
-/*
- * NOTE:
- * Torvalds would NOT put global variables in a header,
- * much less initialized ones. So this is only declared.
- */
-extern char ranks_ip[25];
+    #ifndef DataBase1
+    #define DataBase1 "IP2LOCATION-LITE-DB1.csv"
 
-/* Function prototypes */
-bool search(int rank);
-struct ip ip_string(struct ip ip, char *line);
-int database_num(struct ip ip);
+    #define size_line 78 
 
-#endif /* _BUSCAR_H */
+        /*
+         * struct line - single range entry for database lookup
+         * @lower_rank: start of IP range (numeric)
+         * @upper_rank: end of IP range (numeric)
+         * @country:    country name
+         * @code:       2-letter country code
+         * @latitude:   latitude (for DB5/DB9/DB11)
+         * @longitude:  longitude (for DB5/DB9/DB11)
+         */
+
+        struct line {
+            int   lower_rank;
+            int   upper_rank;
+            char  country[53];
+            char  code[3];
+            float latitude;
+            float longitude;
+        };
+
+    #endif
+
+    #ifndef DataBase3
+    #define DataBase3 "IP2LOCATION-LITE-DB3.csv"
+
+
+    #endif
+
+    #ifndef DataBase5
+    #define DataBase5 "IP2LOCATION-LITE-DB5.csv"
+
+    #endif
+
+    #ifndef DataBase9
+    #define DataBase9 "IP2LOCATION-LITE-DB9.csv"
+
+    #endif
+
+    #ifndef DataBase11
+    #define DataBase11 "IP2LOCATION-LITE-DB11.csv"
+
+    #endif
+
+    extern char ranks_ip[25];
+
+    /* Function prototypes */
+
+    bool search(int rank);
+    struct ip ip_string(struct ip ip, char *line);
+    int database_num(struct ip ip);
+
+#endif 
